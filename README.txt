e314457a5c47297b999a3de8b20586c709df610b
skynet10 <fakerplayer044@gmail.com>
2024-01-19 13:09:02 -0300

    web scraping

---
+++ README.txt
@@ -0,0 +1,15 @@
+Readme File
+Github:https://github.com/skynet10
+
+Web Spiders is a web scraping tool developed in python that can work 
+with lists of urls,allowing you to scrape multiple websites at the same time. 
+For the tool to work you need to install the necessary modules, 
+which are in the requirements file.txt using pip [pip install -r requirements.txt]. 
+After installing the modules, you can start the tool (./spiders.py) 
+by entering the file with the list of sites you want to scrape [.spiders.py list].
+In case of any error the program will show on the screen.
+
+
+
+
+
---
+++ spiders.py
@@ -0,0 +1,51 @@
+#!/bin/python3
+
+import requests,sys,re
+from bs4 import BeautifulSoup
+from pyfiglet import Figlet
+from termcolor import colored
+
+f = Figlet(font ="standard")
+print(colored(f.renderText("Web Spiders"),"green"))
+print("Github:https://github.com/skynet10\n")
+info =  input("Type in what you want to search for: ").strip()
+
+try:
+    args = sys.argv[1]
+    with open(args,"r") as txt:
+       txt = txt.readlines()
+       for t in txt:
+            t = t.strip()
+            headers={"User-Agent":"Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0"}
+            r = requests.get(t,headers=headers)
+            soup = (BeautifulSoup(r.content,"html.parser",))
+            x = "Response"
+            print(f"\n{x:-<100}\n" + f"{t} -----> Status:{r.status_code}\n")
+            body = soup.find_all(info)
+
+            for y in body:
+                    respost = re.compile(r"https?://([a-zA-Z0-9])([./a-zA-z0-9])*([./a-zA-Z0-9])*([./a-zA-Z0-9])*")
+                    response = respost.finditer(str(y))
+                    for z in response:
+                         print(f"{y}\n")
+
+            for z in body:
+                     respost = re.compile(r"([/.a-zA-Z0-9])([./a-zA-z0-9])*([./a-zA-Z0-9])*([./a-zA-Z0-9])*")
+                     response = respost.finditer(str(z))
+                     for z in response:
+                          print(f"{z}")
+
+except IndexError:
+      print("\nEnter a file!\n")
+
+except requests.exceptions.InvalidSchema:
+      print("\nThe file contains invalid URLs!\n")
+
+except requests.exceptions.MissingSchema:
+      print("\nInvalid file!\n")
+
+except ModuleNotFoundError:
+      print("Modules not detected!. Install the file requirements.txt [pip install -r requirements.txt]")
+
+
+
